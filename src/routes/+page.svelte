08f9879e61b97a1918c5de6c9a77ffe4432ce432
Benjamin Modave <b.modave@astroscale.com>
2026-02-02 16:03:18 +0900

    update all errors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-	import MapView from '$lib/components/MapView.svelte';
-	import BottomNavigation from '$lib/components/BottomNavigation.svelte';
-	import CircularDialog from '$lib/components/CircularDialog.svelte';
+	import MapView from '$lib/components/map/MapView.svelte';
+	import BottomNavigation from '$lib/components/nav/BottomNavigation.svelte';
+	import AuthDialog from '$lib/components/nav/AuthDialog.svelte';
 </script>
 
 <main class="app-container">
-	<CircularDialog />
+	<AuthDialog />
 	<MapView />
 	<BottomNavigation />
 </main>
